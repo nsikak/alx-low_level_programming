d73e55b0008d58bc757ed3138dd9ad89b3af6d9a
nsikak <insycarc@gmail.com>
2023-08-14 16:31:13 -0700

    new

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Print jargons
+ * Description: Just a fucntion
+ * Return: void
+ */
+
+int main(void)
+{
+	printf('with proper grammar, but the outcome is a piece of art,');
+	return (0);
+}
