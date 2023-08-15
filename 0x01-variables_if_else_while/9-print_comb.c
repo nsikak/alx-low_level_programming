6a2101b5fae4185b7e1938024e45bb3e51297aaa
nsikak <insycarc@gmail.com>
2023-08-14 18:01:01 -0700

    new

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Main code
+ * Description:nothing
+ * Return: void
+ */
+
+int main(void)
+{
+	int z;
+
+	for (z = 0; z < 10; z++)
+	{
+		putchar(z + '0');
+		if (z < 9)
+			putchar(',');
+	}
+	putchar('\n');
+	return (0);
+}
