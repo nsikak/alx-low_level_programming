fb5956b66e1069f02cbfbf613dc9570b28d2ff88
nsikak <insycarc@gmail.com>
2023-08-14 17:41:07 -0700

    new

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Main code
+ * Description:nothing
+ * Return: void
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i != 'q' || i != 'e')
+			putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
