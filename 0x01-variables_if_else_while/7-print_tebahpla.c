f0a1a7d27379c9319ba73aa78175dbc1e342da54
nsikak <insycarc@gmail.com>
2023-08-14 17:51:20 -0700

    new

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Main code
+ * Description:nothing
+ * Return: void
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'z'; i <= 'a'; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
