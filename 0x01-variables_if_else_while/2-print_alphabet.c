9c906fd80b0ec967cf42de624fab42ee9141fef4
nsikak <insycarc@gmail.com>
2023-08-14 17:32:43 -0700

    new

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Main code
+ * Description:nothing
+ * Return: void
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
