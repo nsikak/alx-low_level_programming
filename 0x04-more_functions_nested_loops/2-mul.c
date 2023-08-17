e68987c725b5314ef6273f47f6c4c186985e10d3
nsikak <insycarc@gmail.com>
2023-08-17 16:19:41 -0700

    2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - a function toprint upper
+ *Description: None
+ *Return: 1
+ *@a: first num
+ *@b: second num
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,6 +2,7 @@
 #define main_h
 
 int _isupper(int c);
+int mul(int a, int b);
 int _isdigit(int c);
 
 #endif 
