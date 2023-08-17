08d3f8bdce90e306c48420865463d7ef40eeac37
nsikak <insycarc@gmail.com>
2023-08-17 16:11:53 -0700

    2

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isdigit - a function toprint upper
+ *Description: None
+ *Return: 1
+ *@c: char
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <=  57)
+		return (1);
+	else
+		return (0);
+}
