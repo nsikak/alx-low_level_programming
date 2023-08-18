39050586a63e02cd7ffb3abd62f05c881d73f028
nsikak <insycarc@gmail.com>
2023-08-17 18:22:18 -0700

    2

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_triangle - a function toprint upper
+ *Description: None
+ *Return: 1
+ *@size: num of lines
+ */
+
+void print_triangle(int size)
+{
+	int i;
+
+	for (i = 1; i <= size; i++)
+	{
+		int z;
+
+		z = 0;
+		while (z < size)
+		{
+			if (z < size - i && size > 1)
+				_putchar(32);
+			else
+				_putchar(35);
+			z++;
+		}
+		
+		if (i != size)
+			_putchar('\n');
+	}
+	_putchar('\n');
+}
