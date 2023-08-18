b994eb65659422e03d700429dfdde81c9d548cfa
nsikak <insycarc@gmail.com>
2023-08-17 18:42:17 -0700

    2

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - a function toprint upper
+ *Description: None
+ *Return: 1
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 > 0 && i % 5 > 0)
+		{
+			printf("%d ", i);
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+		{
+			printf("Buzz ");
+		}
+	}
+	return (0);
+}
