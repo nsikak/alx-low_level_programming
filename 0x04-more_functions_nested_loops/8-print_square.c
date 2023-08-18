36d9661d91bd4b50d89004ef79630dabae400cb4
nsikak <insycarc@gmail.com>
2023-08-17 17:52:53 -0700

    2

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square - a function toprint upper
+ *Description: None
+ *Return: 1
+ *@size: num of lines
+ */
+
+void print_square(int size)
+{
+	int i;
+
+	for (i = 1; i <= size; i++)
+	{
+		int z;
+
+		for (z = 1; z <= size; z++)
+		{
+			_putchar('#');
+		}
+		if (i != size)
+			_putchar('\n');		
+	}	
+	_putchar('\n');
+}
