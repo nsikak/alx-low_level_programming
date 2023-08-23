5f8b383fa72b97a8c83dfcce72cc0e65b403214a
nsikak <insycarc@gmail.com>
2023-08-22 21:05:00 -0700

    ponters

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - resets a pointer
+ * Description: Just a function
+ * Return: 0
+ * @str: pointer params
+ */
+
+void _puts(char *str)
+{
+
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+
+}
