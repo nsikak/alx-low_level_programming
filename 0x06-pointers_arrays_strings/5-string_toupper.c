313ebbc130bbb23bee532d53af445fef00d92df4
nsikak <insycarc@gmail.com>
2023-08-24 17:06:38 -0700

    b

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+/**
+ * string_toupper - strcat
+ *Description: just a func
+ *Return: char
+ *@s: src
+ */
+
+char *string_toupper(char *s);
+{
+	int i = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] += 32;
+	}
+	return (s);
+
+}
