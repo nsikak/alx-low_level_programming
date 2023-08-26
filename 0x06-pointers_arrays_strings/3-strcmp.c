3ff8ec885c12eb9903a007eafe6c377fecbcdfcc
nsikak <insycarc@gmail.com>
2023-08-25 21:57:30 -0700

    a

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,16 +8,20 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i;
-
-	while (s1[i] != '\0' && s2[i] != '\0')
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
 		if (s1[i] != s2[i])
 		{
 			return (s1[i] - s2[i]);
 		}
-		i++;
+		else if (s1[i] == '\t' && s2[i] == '\t')
+		{
+			continue;
+		}
+		else
+		{
+			return (s1[i] - s2[i]);
+		}
 	}
-
 	return (s1[i] - s2[i]);
 }
