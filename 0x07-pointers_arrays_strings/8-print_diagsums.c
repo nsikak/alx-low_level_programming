5b97f1a950fd44a05f3b7175312983c2f104219a
nsikak <insycarc@gmail.com>
2023-08-29 21:45:03 -0700

    a

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_diagsums - strcat
+ *Description: just a func
+ *Return: char
+ *@a: dest
+ *@size: src
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i, j, sum1 = 0, sum2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		sum1 += a[i * size + i];
+		sum2 += a[i * size + (size - i - 1)];
+	}
+
+	printf("%d, %d\n", sum1, sum2);
+}
