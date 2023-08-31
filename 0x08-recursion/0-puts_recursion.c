90ab8f2b362a43fdb959a6610c2dbd00a478ff10
nsikak <insycarc@gmail.com>
2023-08-31 15:21:10 -0700

    f

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#includes "main.h"
+
+/**
+ *_puts_recursion - a recursive finction
+ *Description: Recursion
+ *Returns: null
+ *@s: s
+ */
+
+void _puts_recursion(char *s)
+{
+	while (*s != '\0')
+		_putchar(*s);
+		s++;
+	_putchar('\n');
+}
