5e6219b3bcb55286a329439b2ac0fa50ece2469e
nsikak <insycarc@gmail.com>
2023-08-31 16:47:54 -0700

    f

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *factorial - a recursive finction
+ *Description: Recursion
+ *Return: null
+ *@n: source
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
