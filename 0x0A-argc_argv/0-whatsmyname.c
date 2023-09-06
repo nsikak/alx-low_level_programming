4806274d088e69c1de508ce9ecc11b6b8f0e2eca
nsikak <insycarc@gmail.com>
2023-09-06 16:36:22 -0700

    a

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main- arguments
+ *Description: a
+ *Return: 0
+ *@argc: count of arg
+ *@argv: argument array
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s", argv[0]);
+	return (0);
+}
