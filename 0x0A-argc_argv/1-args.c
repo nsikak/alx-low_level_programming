95357357e19eecfa4a0b57bb1701d74d754eb411
nsikak <insycarc@gmail.com>
2023-09-06 16:41:57 -0700

    a

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main- arguments
+ *Description: a
+ *Return: 0
+ *@argc: count of arg
+ *@argv: argument array
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	return (argc - 1);
+}
